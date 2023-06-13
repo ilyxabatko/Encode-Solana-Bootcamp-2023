1ee3c16d6f4b4eb8724dc18be00b647bd4f7e288
ilyxabatko <ilyxabatko@inbox.ru>
2023-06-13 15:07:10 +0400

    anchor homework, test passes

--- week3-4/homework12+/hello-world/programs/hello-world/src/lib.rs
+++ week3-4/homework12+/hello-world/programs/hello-world/src/lib.rs
@@ -7,7 +7,9 @@ pub mod hello_world {
     use super::*;
 
     pub fn initialize(ctx: Context<ChangeData>) -> Result<()> {
+        msg!("Amount before initializing: {}", ctx.accounts.account_to_change.amount);
         ctx.accounts.account_to_change.amount = 100;
+        msg!("Amount after initializing: {}", ctx.accounts.account_to_change.amount);
 
         Ok(())
     }
@@ -15,13 +17,20 @@ pub mod hello_world {
 
 #[derive(Accounts)]
 pub struct ChangeData<'info> {
+    #[account(init, payer = payer, space = MyAccount::SIZE)]
+    pub account_to_change: Account<'info, MyAccount>,
     #[account(mut)]
-    pub account_to_change: Account<'info, MyAccount>
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(Default)]
 pub struct MyAccount {
     amount: u64
+}
+
+impl MyAccount {
+    const SIZE: usize = 8 + 8;
 }
 
--- week3-4/homework12+/hello-world/tests/hello-world.ts
+++ week3-4/homework12+/hello-world/tests/hello-world.ts
@@ -4,23 +4,29 @@ import { HelloWorld } from "../target/types/hello_world";
 import { assert } from "chai";
 
 describe("hello-world", () => {
-    anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
     const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
 
     it("Initializes account data correctly", async () => {
+        // create and init an account
         const account = anchor.web3.Keypair.generate();
+        const payer_keypair = provider.wallet;
 
         const tx = await program.methods.initialize()
-        .accounts({
-            accountToChange: account.publicKey
-        })
-        .rpc();
+            .accounts({
+                accountToChange: account.publicKey,
+                payer: payer_keypair.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId
+            })
+            .signers([account])
+            .rpc();
 
-        const initializedBalance = (await program.account.myAccount.fetch(account.publicKey)).amount;
+        const initializedBalance = ((await program.account.myAccount.fetch(account.publicKey)).amount).toNumber();
 
-        const expectedBalace = new anchor.BN(100);
+        const expectedBalance: number = 100;
         console.log("Your transaction signature", tx);
-        assert.equal(expectedBalace, initializedBalance);
+        assert.equal(expectedBalance, initializedBalance);
     });
 });
